39fb0d9e44eb4b87d670c74b51aefa2365ded10c
zander <23585137@protonmail.ch>
2017-05-14 10:13:55 +0200

    Progress bars are now themed but still very basic. Considering removing progress bars and multithreading support since it causes many problems or at least maintaining it on a different branch. Included resource files in version control.

--- src/cryogen/Cryptogen.java
+++ src/cryogen/Cryptogen.java
@@ -288,7 +288,7 @@ public class Cryptogen implements Initializable
 
             encryptFiles(files);
 
-            /*Alert encryptionInformation = new Alert(Alert.AlertType.INFORMATION, message + method + message2);
+            Alert encryptionInformation = new Alert(Alert.AlertType.INFORMATION, message + method + message2);
             encryptionInformation.setGraphic(new ImageView(this.getClass().getResource("/icons/success32.png").toString()));
             Button okButton = (Button) encryptionInformation.getDialogPane().lookupButton(ButtonType.OK);
             okButton.setText("OK");
@@ -302,7 +302,7 @@ public class Cryptogen implements Initializable
             if (!ButtonType.OK.equals(closeResponse.get()))
             {
                 event.consume();
-            }*/
+            }
         }
         catch (NoFilesAttachedException ex)
         {
